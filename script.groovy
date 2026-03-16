3d60d33c77a1a5e30b8eff28937939bf5ea0a693
zengshusen <1795183945@qq.com>
2026-03-16 20:29:22 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,9 @@
+#!/user/bin/env groovy
+library identifier: 'jenkins-shared-library@master',retriever:modeSCM(
+    [$class:'GitSCMSource',
+     remote:'https://github.com/zengshusen/jenkins-shared-library.git'
+     credentialsId:'github-credentials'])
+@library('jenkins-shared-library@2.0')
 def buildApp() {
     echo 'building the application...'
 }
